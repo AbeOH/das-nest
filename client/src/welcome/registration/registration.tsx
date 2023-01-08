ca80765767a3fd47b92e1abfb38ddbdbe9ed52ed
Abraham Hdru <abraham.hdru@gmail.com>
2023-01-08 15:03:52 +0100

    Landing and login

--- client/src/welcome/registration/registration.tsx
+++ client/src/welcome/registration/registration.tsx
@@ -1,23 +1,62 @@
 import { Component, FormEvent } from "react";
 
-export class Registration extends Component {
+interface RegistrationState {
+    firstname?: string;
+    lastname?: string;
+    email?: string;
+    password?: string;
+    errormsg?: string;
+}
+
+export class Registration extends Component<any, RegistrationState> {
     constructor(probs) {
         super(probs);
         this.state = {
-            firstName: "",
-            lastName: "",
+            firstname: "",
+            lastname: "",
             email: "",
             password: "",
+            errormsg: "",
         };
     }
     handleInputChange = (evt) => {
-        const property = evt.target.name; // This line will hold firstname when input for firstname is changed
-        this.setState({ [property]: evt.target.value }); // This line will update firstname prob dynamically in this.state variable
+        const property = evt.target.name; // This line will hold value when input for value is changed
+        const value = evt.target.value;
+        this.setState({ [property]: value }); // This line will update value prob dynamically in this.state variable
     };
     handleSubmit = (evt: FormEvent) => {
         evt.preventDefault();
+        const { firstname, lastname, email, password } = this.state;
 
-        // Implement: Post request with fetch to server
+        fetch("/register", {
+            method: "POST",
+            headers: {
+                "Content-/Type": "application/json",
+            },
+            body: JSON.stringify({
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                password: password,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log("data: ", data);
+                if (data.success) {
+                    location.reload();
+                } else {
+                    this.setState({
+                        errormsg:
+                            "Something went wrong. Please try again later or with a different email",
+                    });
+                }
+            })
+            .catch(() => {
+                this.setState({
+                    errormsg: "Something went wrong. Please try again later",
+                });
+            });
     };
     render() {
         console.log("state: ", this.state);
@@ -30,24 +69,49 @@ export class Registration extends Component {
                     <div>
                         <span>Firstname</span>
                         <input
+                            required
+                            type="text"
                             name="firstname"
+                            // value = {this.state.firstname} // Need to figure out how to update value dynamically
                             onChange={this.handleInputChange}
                         />
                     </div>
                     <div>
                         <span>Lastname</span>
-                        {/* <input /> */}
+                        <input
+                            required
+                            type="text"
+                            name="lastname"
+                            // value={this.state.lastname}
+                            onChange={this.handleInputChange}
+                        />
                     </div>
                     <div>
                         <span>Email</span>
-                        {/* <input /> */}
+                        <input
+                            required
+                            type="email"
+                            name="email"
+                            // value={this.state.email}
+                            onChange={this.handleInputChange}
+                        />
                     </div>
                     <div>
                         <span>Password</span>
-                        {/* <input /> */}
+                        <input
+                            required
+                            type="password"
+                            name="password"
+                            // value={this.state.password}
+                            onChange={this.handleInputChange}
+                        />
                     </div>
                     <button>Register</button>
                 </form>
+                <p>
+                    If you already have an account, please
+                    <a href="/login"> login here </a>
+                </p>
             </div>
         );
     }
--- server/server.js
+++ server/server.js
@@ -4,12 +4,32 @@ const compression = require("compression");
 const path = require("path");
 const { PORT = 3001 } = process.env;
 
+// Importing functions from db.js
+const { addUser } = require("../db.js");
+
 app.use(compression());
 // Need to use the cookie-session middleware
 // Need json middleware for POST requests
 
+// Setting up cookies session
+const cookieSession = require("cookie-session");
+
+// Importing function from bcrypt.js
+const { hash, compare } = require("../brypt.js");
+
+app.use(
+    cookieSession({
+        secret: "It takes a village to raise a child",
+        maxAge: 1000 * 60 * 60 * 24 * 14,
+    })
+);
+// Setting up json middleware == Enables access values in req.body
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
+//***************************************************************************************** */
+// Get Routes
 app.get("/user/id.json", (req, res) => {
     res.json({ userId: null }); // Instead of null; I need to use value from req.session.userId
 });
@@ -21,4 +41,34 @@ app.get("*", function (req, res) {
 
 app.listen(PORT, function () {
     console.log(`Express server listening on port ${PORT}`);
+});
+
+//***************************************************************************************** */
+// Post Routes for Registration and Login
+
+app.post("/registration", (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    const password = req.body.password;
+    hash(password).then((hashed) => {
+        if (
+            firstname !== "" &&
+            lastname !== "" &&
+            email !== "" &&
+            password !== ""
+        ) {
+            addUser(firstname, lastname, email, hashed)
+                .then((data) => {
+                    // {rows}
+                    console.log("rows: ", data.rows);
+                    req.session.userId = data.rows[0].id;
+                    res.json({ success: true });
+                })
+                .catch((err) => {
+                    console.log("error in POST /registration: ", err);
+                    res.json({ success: false });
+                });
+        }
+    });
 });
