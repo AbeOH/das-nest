c8ec25d2213178192b00f9323178fc6a88c69482
Abraham Hdru <abraham.hdru@gmail.com>
2023-01-16 12:15:22 +0100

    Find component

---
+++ client/src/components/find/find.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Find() {
+    const [users, setUsers] = useState<{ name: string }[]>([]);
+    const [search, setSearch] = useState("");
+    const [searchResults, setSearchResults] = useState<{ name: string }[]>([]);
+
+    // const handleSearch = (users: { name: string }[]) => {
+    //     const filteredUsers = users.filter((user) => {
+    //         // return
+    //         user.name.toLowerCase().includes(search.toLowerCase());
+    //     });
+    //     setSearchResults(filteredUsers);
+    // };
+
+    useEffect(() => {
+        fetch("/users?search=" + search)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("data from server: ", data);
+                setSearchResults(data);
+            })
+            .catch((err) => {
+                console.log("Errrrrrror in fetch: ", err);
+            });
+    }, [searchResults]);
+
+    return (
+        <section>
+            <div className="search">
+                <h2>Search Friends</h2>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    onChange={(evt: React.ChangeEvent<HTMLInputElement>) => {
+                        setSearch(evt.target.value);
+                    }}
+                />
+            </div>
+            <div></div>
+        </section>
+    );
+}
