b919a15e11be77661a5f53786c5b698e06ec96f0
Abraham Hdru <abraham.hdru@gmail.com>
2023-01-22 16:13:53 +0100

    Redux slicer  & reduer for friendlist

---
+++ client/src/app/typeinterface.ts
@@ -0,0 +1,15 @@
+export type Action = {
+    type: string;
+    payload: any;
+};
+
+export interface FriendListState {
+    id: number;
+    sender_id: number;
+    recipient_id: number;
+    accepted: boolean;
+    firstname: string;
+    lastname: string;
+    imageurl: string;
+    bio: string;
+}
